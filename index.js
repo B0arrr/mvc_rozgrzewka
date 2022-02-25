77ae38dcf6b10b517e7cb8308c37d286e93b5cbb
Kowal031 <73485596+Kowal031@users.noreply.github.com>
2022-02-25 13:56:12 +0100

    Add wireframe pages
    Can't connect pages

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 let express = require('express')
 let app = express()
-const flash = require('connect-flash')
 const path = require('path')
-const session = require('express-session')
-const mongoose = require("mongoose")
 const port = 2137
 
-const database = 'mongodb://localhost:27017/mvc_rozgrzewka_db?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
-mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true})
-    .then(() => console.log('Poloczono'))
-    .catch(err => console.log(err))
-
 app.set('view engine', 'pug')
-
 app.use(express.urlencoded({extended: false}))
-
-app.use(flash())
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}))
-
 app.use('/', require('./routers/router'))
 
 app.use(express.static(path.join(__dirname, 'public')))
